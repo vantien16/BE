0acf8572ee62cbe3a5fad557deab05b3d5cdd43e
Tien Bug <145079654+vantien16@users.noreply.github.com>
2023-11-20 23:56:36 +0700

    Done99+like details

--- src/main/java/com/petlover/petsocial/service/ReactionService.java
+++ src/main/java/com/petlover/petsocial/service/ReactionService.java
@@ -4,6 +4,7 @@ import com.petlover.petsocial.exception.PostException;
 import com.petlover.petsocial.exception.UserException;
 import com.petlover.petsocial.payload.request.ReactionDTO;
 import com.petlover.petsocial.payload.request.UserDTO;
+import com.petlover.petsocial.payload.request.UserPostDTO;
 
 import java.util.List;
 
@@ -11,4 +12,5 @@ public interface ReactionService {
     ReactionDTO reactionPost(Long idPost, UserDTO userDTO) throws UserException, PostException;
     List<ReactionDTO> getAllReaction(Long idPost) throws PostException;
     ReactionDTO reactComment(Long idComment, UserDTO userDTO) throws UserException;
+    List<UserPostDTO> getAllUserReaction(Long idPost) throws PostException;
 }
